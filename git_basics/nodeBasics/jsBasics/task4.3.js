6e4c2a12cc1033037d79a5aba44839796cb249a2
OSKovalchuk <Olga.Kovalchuk@symphony-solutions.com>
2025-05-01 02:12:50 +0300

    Add basic JS tasks: variables, math operations, rounding

---
+++ git_basics/nodeBasics/jsBasics/task4.3.js
@@ -0,0 +1,13 @@
+// Create a variable for the radius of the cylinder
+let radius = 2;
+
+// Create a variable for the height of the cylinder
+let height = 5;
+
+// Calculate the volume of the cylinder using the formula π * radius^2 * height
+let volume = Math.PI * radius ** 2 * height;
+
+ //Output the result to the console
+ console.log("Radius:", radius.toFixed(2));
+ console.log("Height:", height.toFixed(2));
+ console.log("Volume of the cylinder:", volume.toFixed(2));
