563b524e3e163d14ad694b5ff51ad7126ca5e0e2
OSKovalchuk <Olga.Kovalchuk@symphony-solutions.com>
2025-05-09 17:32:06 +0300

    Add Homework_6

---
+++ homework_6/task_2.js
@@ -0,0 +1,13 @@
+
+function isAdult(age) {
+    return age >= 18;
+  }
+  
+  
+  const result1 = isAdult(25);
+  const result2 = isAdult(15);
+  
+  
+  console.log("Is 25 years old an adult?", result1); // true
+  console.log("Is 15 years old an adult?", result2); // false
+  
